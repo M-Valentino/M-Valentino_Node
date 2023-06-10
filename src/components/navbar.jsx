2c1ff242ded9bef0489a5dd1a66888ba91f208c4
Mark Valentino <mike9377mike9377@gmail.com>
2023-06-10 19:40:48 -0400

    added active color to navbar links

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,6 @@
 import * as React from "react";
 import Link from "next/link";
 import AppBar from "@mui/material/AppBar";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 import Button from "@mui/material/Button";
 import { Stack, Typography } from "@mui/material";
 import ICButton from "./icButton";
