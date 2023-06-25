4dad6264cfb0e614565ad9cf4c290ae73153f734
Mark Valentino <mike9377mike9377@gmail.com>
2023-06-25 19:36:29 -0400

    added spiring animation

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,9 +1,7 @@
-import * as React from "react";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
+import React from "react";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
-import Grow from "@mui/material/Grow";
+import { useSpring, animated } from "@react-spring/web";
 import { ThemeProvider } from "@mui/material/styles";
 import { MainTheme } from "@/utils/MUITheme";
 import { LanguageChip } from "./LanguageChip";
@@ -11,6 +9,17 @@ import { LANGUAGES } from "@/consts/projectContent";
 
 export default function ProjectCard(props) {
   const { href, imageLink, title, date, description, languages } = props.data;
+
+  const springs = useSpring({
+    from: { y: -500, opacity: 0},
+    to: { y: 0, opacity: 1 },
+    delay: props.index * 100 ,
+    config: {
+      mass: 1,
+      friction: 19
+    },
+
+  });
 
   /**
    * Function for determining which background color the coding language tags will have
@@ -37,39 +46,44 @@ export default function ProjectCard(props) {
   return (
     <ThemeProvider theme={MainTheme}>
       {/* Cards on the projects page will appear one after another. Cards with a lower index will apear sooner. */}
-      <Grow in timeout={250 * props.index + 250}>
-        <Card sx={{ maxWidth: 345 }}>
-          <a style={{ textDecoration: "none" }} href={href}>
-            <CardContent>
-              <img src={imageLink} style={{ width: "100%" }}></img>
-              <Stack direction="row" spacing={1}>
-                <Typography variant="h6" color="primary">
-                  {date}
-                </Typography>
-                <Typography variant="h5" color="text.primary">
-                  {title}
-                </Typography>
-              </Stack>
-              <Typography
-                variant="body2"
-                color="text.secondary"
-                style={{ textAlign: "justify" }}
-              >
-                {description}
-              </Typography>
-              <Stack direction="row" spacing={1} mt={1}>
-                {languages.map((props, index) => (
-                  <LanguageChip
-                    index={index}
-                    label={props}
-                    backgroundColor={getChipColor(props)}
-                  />
-                ))}
-              </Stack>
-            </CardContent>
-          </a>
-        </Card>
-      </Grow>
+
+      <animated.div
+        style={{
+          backgroundColor: "#fff",
+          width: 360,
+          padding: 24,
+          borderRadius: 4,
+          ...springs,
+        }}
+      >
+        <a style={{ textDecoration: "none" }} href={href}>
+          <img src={imageLink} style={{ width: "100%", borderRadius: 2 }}></img>
+          <Stack direction="row" spacing={1}>
+            <Typography variant="h6" color="primary">
+              {date}
+            </Typography>
+            <Typography variant="h5" color="text.primary">
+              {title}
+            </Typography>
+          </Stack>
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            style={{ textAlign: "justify" }}
+          >
+            {description}
+          </Typography>
+          <Stack direction="row" spacing={1} mt={1}>
+            {languages.map((props, index) => (
+              <LanguageChip
+                index={index}
+                label={props}
+                backgroundColor={getChipColor(props)}
+              />
+            ))}
+          </Stack>
+        </a>
+      </animated.div>
     </ThemeProvider>
   );
 }
--- src/components/_app.js
+++ src/components/_app.js
@@ -1,6 +1,3 @@
-import '@/styles/globals.css'
-import NavBar from './navbar';
-
 export default function App({ Component, pageProps }) {
   return (
     <Component {...pageProps} />
