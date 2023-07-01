57bbf92bfaefa163909f060329bf9943eb13742b
Mark Valentino <mike9377mike9377@gmail.com>
2023-07-01 15:27:44 -0400

    did code cleanup of the imports

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,17 +1,22 @@
 import Head from "next/head";
 import React, { useState } from "react";
-import { Grid, Stack, TextField } from "@mui/material";
-import Typography from "@mui/material/Typography";
-import { ThemeProvider } from "@mui/material/styles";
+import {
+  Grid,
+  Stack,
+  TextField,
+  ThemeProvider,
+  ToggleButton,
+  ToggleButtonGroup,
+  Typography,
+} from "@mui/material";
+import ViewListIcon from "@mui/icons-material/ViewList";
+import ViewModuleIcon from "@mui/icons-material/ViewModule";
 import { MainTheme } from "@/utils/MUITheme";
 import NavBar from "@/components/navbar";
 import ProjectCard from "@/components/ProjectCard";
 import ProjectTable from "@/components/ProjectTable";
 import { projectContent } from "@/consts/projectContent";
-import ViewListIcon from "@mui/icons-material/ViewList";
-import ViewModuleIcon from "@mui/icons-material/ViewModule";
-import ToggleButton from "@mui/material/ToggleButton";
-import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+
 
 export default function projects() {
   const [view, setView] = useState("cardView");
@@ -62,11 +67,7 @@ export default function projects() {
                 <ViewListIcon /> Table View
               </ToggleButton>
             </ToggleButtonGroup>
-            <TextField
-              label="Helper text"
-              variant="standard"
-              fullWidth
-            />
+            <TextField label="Helper text" variant="standard" fullWidth />
           </Stack>
           {view === "cardView" && (
             <Grid
