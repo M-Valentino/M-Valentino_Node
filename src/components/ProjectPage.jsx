8917c4d76f9e22184de778ddb3cfdd3835349f56
Mark Valentino <mike9377mike9377@gmail.com>
2023-07-03 17:11:09 -0400

    got mui theme and navbar on dynamic links

--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import { Stack, Typography, useMediaQuery } from "@mui/material";
-import { ThemeProvider } from "@mui/material/styles";
-import { MainTheme } from "@/utils/MUITheme";
 import { LanguageChip } from "./LanguageChip";
 import { MOBILE_WIDTH, DESKTOP_WIDTH } from "@/consts/stylingValues";
 
@@ -17,52 +15,47 @@ export default function ProjectPage(props) {
   } = props.data;
   const isDesktopView = useMediaQuery("(min-width:900px)");
   return (
-    <ThemeProvider theme={MainTheme}>
-     
-        <div
-          style={{
-            backgroundColor: "#fffdfa",
-            width: isDesktopView? DESKTOP_WIDTH : MOBILE_WIDTH,
-            magin: 'auto',
-            borderRadius: 4,
-            textRendering: "geometricPrecision!important",
-          }}
+    <div
+      style={{
+        backgroundColor: "#fffdfa",
+        width: isDesktopView ? DESKTOP_WIDTH : MOBILE_WIDTH,
+        magin: "auto",
+        borderRadius: 4,
+        textRendering: "geometricPrecision!important",
+      }}
+    >
+      <img
+        src={imageLink}
+        style={{
+          borderRadius: 2,
+          boxShadow: "0 1px 1px rgba(0, 0, 0, 0.1)",
+        }}
+      ></img>
+      <Stack direction="row" spacing={1}>
+        <Typography variant="h6" color="primary">
+          {date}
+        </Typography>
+        <Typography
+          variant={smallerTitleFontNeeded ? "h6" : "h5"}
+          color="text.primary"
         >
-          <img
-            src={imageLink}
-            style={{
-              
-              borderRadius: 2,
-              boxShadow: "0 1px 1px rgba(0, 0, 0, 0.1)",
-            }}
-          ></img>
-          <Stack direction="row" spacing={1}>
-            <Typography variant="h6" color="primary">
-              {date}
-            </Typography>
-            <Typography
-              variant={smallerTitleFontNeeded ? "h6" : "h5"}
-              color="text.primary"
-            >
-              {title}
-            </Typography>
-          </Stack>
-          <div style={{ height: 145 }}>
-            <Typography
-              variant="body2"
-              color="text.secondary"
-              style={{ textAlign: "justify" }}
-            >
-              {description}
-            </Typography>
-          </div>
-          <Stack direction="row" spacing={1} mt={1}>
-            {languages.map((props, index) => (
-              <LanguageChip index={index} language={props} size="full" />
-            ))}
-          </Stack>
-        </div>
-      
-    </ThemeProvider>
+          {title}
+        </Typography>
+      </Stack>
+      <div style={{ height: 145 }}>
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          style={{ textAlign: "justify" }}
+        >
+          {description}
+        </Typography>
+      </div>
+      <Stack direction="row" spacing={1} mt={1}>
+        {languages.map((props, index) => (
+          <LanguageChip index={index} language={props} size="full" />
+        ))}
+      </Stack>
+    </div>
   );
 }
--- src/pages/project/index.jsx
+++ src/pages/project/index.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import NavBar from "@/components/navbar";
+import { ThemeProvider } from "@mui/material/styles";
+import { MainTheme } from "@/utils/MUITheme";
 import ProjectPage from "@/components/projectPage";
 import { projectContent } from "@/consts/projectContent";
 
@@ -6,11 +9,13 @@ class Test extends React.Component {
   static async getInitialProps({ query }) {
     return { query };
   }
-  
 
   render() {
     return (
-      <ProjectPage data={projectContent[this.props.query.projectID]} />
+      <ThemeProvider theme={MainTheme}>
+        <NavBar />
+        <ProjectPage data={projectContent[this.props.query.projectID]} />
+      </ThemeProvider>
     );
   }
 }
