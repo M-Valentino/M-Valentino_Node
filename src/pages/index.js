a01b689472a4e11d2da9cd0bef3f3f93617c2d88
Mark Valentino <mike9377mike9377@gmail.com>
2023-07-23 16:37:51 -0400

    improved responsiveness of stack icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -55,101 +55,101 @@ export default function Home() {
             </Typography>
             {/* @TODO Map this grid container from JSON */}
             <Grid container spacing={4}>
-              <Grid item md={3} lg={2} xs={4}>
+              <Grid item md={2} xs={3}>
                 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-original-wordmark.svg" />
                 <Typography style={stackIconLabelTextStyle}>AWS</Typography>
               </Grid>
-              <Grid item md={3} lg={2} xs={4}>
+              <Grid item md={2} xs={3}>
                 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/androidstudio/androidstudio-original.svg" />
                 <Typography style={stackIconLabelTextStyle}>
                   Android Studio
                 </Typography>
               </Grid>
-              <Grid item md={3} lg={2} xs={4}>
+              <Grid item md={2} xs={3}>
                 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg" />
                 <Typography style={stackIconLabelTextStyle}>Bash</Typography>
               </Grid>
-              <Grid item md={3} lg={2} xs={4}>
+              <Grid item md={2} xs={3}>
                 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg" />
                 <Typography style={stackIconLabelTextStyle}>C++</Typography>
               </Grid>
-              <Grid item md={3} lg={2} xs={4}>
+              <Grid item md={2} xs={3}>
                 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
                 <Typography style={stackIconLabelTextStyle}>CSS3</Typography>
               </Grid>
-              <Grid item md={3} lg={2} xs={4}>
+              <Grid item md={2} xs={3}>
                 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg" />
                 <Typography style={stackIconLabelTextStyle}>
                   Firebase
                 </Typography>
               </Grid>
-              <Grid item md={3} lg={2} xs={4}>
+              <Grid item md={2} xs={3}>
                 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/handlebars/handlebars-original.svg" />
                 <Typography style={stackIconLabelTextStyle}>
                   Handlebars
                 </Typography>
               </Grid>
-              <Grid item md={3} lg={2} xs={4}>
+              <Grid item md={2} xs={3}>
                 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
                 <Typography style={stackIconLabelTextStyle}>Html5</Typography>
               </Grid>
-              <Grid item md={3} lg={2} xs={4}>
+              <Grid item md={2} xs={3}>
                 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" />
                 <Typography style={stackIconLabelTextStyle}>Java</Typography>
               </Grid>
-              <Grid item md={3} lg={2} xs={4}>
+              <Grid item md={2} xs={3}>
                 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
                 <Typography style={stackIconLabelTextStyle}>
                   Javascript
                 </Typography>
               </Grid>
-              <Grid item md={3} lg={2} xs={4}>
+              <Grid item md={2} xs={3}>
                 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg" />
                 <Typography style={stackIconLabelTextStyle}>Jest</Typography>
               </Grid>
-              <Grid item md={3} lg={2} xs={4}>
+              <Grid item md={2} xs={3}>
                 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jupyter/jupyter-original-wordmark.svg" />
                 <Typography style={{ textAlign: "center", fontSize: 14 }}>
                   Jupyter Notebook
                 </Typography>
               </Grid>
-              <Grid item md={3} lg={2} xs={4}>
+              <Grid item md={2} xs={3}>
                 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg" />
                 <Typography style={stackIconLabelTextStyle}>
                   Material UI
                 </Typography>
               </Grid>
-              <Grid item md={3} lg={2} xs={4}>
+              <Grid item md={2} xs={3}>
                 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" />
                 <Typography style={stackIconLabelTextStyle}>MySQL</Typography>
               </Grid>
-              <Grid item md={3} lg={2} xs={4}>
+              <Grid item md={2} xs={3}>
                 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg" />
                 <Typography style={stackIconLabelTextStyle}>Next JS</Typography>
               </Grid>
-              <Grid item md={3} lg={2} xs={4}>
+              <Grid item md={2} xs={3}>
                 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" />
                 <Typography style={stackIconLabelTextStyle}>Node JS</Typography>
               </Grid>
-              <Grid item md={3} lg={2} xs={4}>
+              <Grid item md={2} xs={3}>
                 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/numpy/numpy-original.svg" />
                 <Typography style={stackIconLabelTextStyle}>NumPy</Typography>
               </Grid>
-              <Grid item md={3} lg={2} xs={4}>
+              <Grid item md={2} xs={3}>
                 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pandas/pandas-original.svg" />
                 <Typography style={stackIconLabelTextStyle}>Pandas</Typography>
               </Grid>
-              <Grid item md={3} lg={2} xs={4}>
+              <Grid item md={2} xs={3}>
                 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />
                 <Typography style={stackIconLabelTextStyle}>Python</Typography>
               </Grid>
-              <Grid item md={3} lg={2} xs={4}>
+              <Grid item md={2} xs={3}>
                 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
                 <Typography style={stackIconLabelTextStyle}>
                   React Native
                 </Typography>
               </Grid>
-              <Grid item md={3} lg={2} xs={4}>
+              <Grid item md={2} xs={3}>
                 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/threejs/threejs-original.svg" />
                 <Typography style={stackIconLabelTextStyle}>
                   three.js
