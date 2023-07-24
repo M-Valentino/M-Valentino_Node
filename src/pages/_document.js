9c6060ec7fae8002a30a143950f743d4170751af
Mark Valentino <mike9377mike9377@gmail.com>
2023-07-23 21:52:24 -0400

    fixed background not covering height of pages

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,9 +5,12 @@ export default function Document() {
     <Html
       lang="en"
       style={{
+        height: "100%",
         backgroundColor: "rgb(237,233,235)",
         backgroundImage:
           "linear-gradient(0deg, rgba(230,231,228,1) 0%, rgba(237,233,235,1) 100%)",
+        backgroundRepeat: "no-repeat",
+        backgroundAttachment: "fixed",
       }}
     >
       <Head />
