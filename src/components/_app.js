1bf602b3d51a2c03eb3fb7b188cc943971c5ce5a
Mark Valentino <mike9377mike9377@gmail.com>
2023-07-29 13:21:37 -0400

    removed ms clarity node module an added clarity as a script

--- src/components/_app.js
+++ src/components/_app.js
@@ -1,9 +1,4 @@
-import { useEffect } from "react";
-import { clarity } from "react-microsoft-clarity";
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    clarity.init("i6z07kdmzf");
-  });
   return <Component {...pageProps} />;
 }
