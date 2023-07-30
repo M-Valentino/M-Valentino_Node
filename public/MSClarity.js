8740eba75facb44ba15f945f49944764509eb02b
Mark Valentino <mike9377mike9377@gmail.com>
2023-07-30 00:23:55 -0400

    added type to script

--- public/MSClarity.js
+++ public/MSClarity.js
@@ -9,4 +9,4 @@
   t.src = "https://www.clarity.ms/tag/" + i;
   y = l.getElementsByTagName(r)[0];
   y.parentNode.insertBefore(t, y);
-})(window, document, "clarity", "script", "i6z07kdmzf");
+})(window, document, "clarity", "script", "i6z07kdmzf"); 
