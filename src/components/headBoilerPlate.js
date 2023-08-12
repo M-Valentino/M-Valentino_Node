f9cce5457f4e2426910c8fff5d212c4a6213d576
Mark Valentino <mike9377mike9377@gmail.com>
2023-08-12 15:16:42 -0400

    renamed contentText to descriptionText

--- src/components/headBoilerPlate.js
+++ src/components/headBoilerPlate.js
@@ -1,7 +1,7 @@
-export const getHeadBoilerPlate = (contentText) => {
+export const getHeadBoilerPlate = (descriptionText) => {
   return (
     <>
-      <meta name="description" content={contentText} />
+      <meta name="description" content={descriptionText} />
       <meta name="author" content="Mark Valentino"/>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
