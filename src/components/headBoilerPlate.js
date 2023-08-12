0e9b7a6e0781e6db3b4e36b43020fdf43bff60d6
Mark Valentino <mike9377mike9377@gmail.com>
2023-08-12 15:15:08 -0400

    added author meta tag

--- src/components/headBoilerPlate.js
+++ src/components/headBoilerPlate.js
@@ -2,6 +2,7 @@ export const getHeadBoilerPlate = (contentText) => {
   return (
     <>
       <meta name="description" content={contentText} />
+      <meta name="author" content="Mark Valentino"/>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
     </>
