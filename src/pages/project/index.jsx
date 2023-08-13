18aa4c1ffe3b397ea0e989200c6280a64f2e84e6
Mark Valentino <mike9377mike9377@gmail.com>
2023-08-12 23:04:19 -0400

    added Head to project pages

--- src/pages/project/index.jsx
+++ src/pages/project/index.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Head from "next/head";
 import NavBar from "@/components/navbar";
 import { ThemeProvider } from "@mui/material/styles";
 import { MainTheme } from "@/utils/MUITheme";
 import ProjectPage from "@/components/ProjectPage";
+import { getHeadBoilerPlate } from "@/components/headBoilerPlate";
 import { PROJECT_CONTENT } from "@/consts/projectContent";
 // todo change function name
 class Test extends React.Component {
@@ -12,10 +14,20 @@ class Test extends React.Component {
 
   render() {
     return (
-      <ThemeProvider theme={MainTheme}>
-        <NavBar />
-        <ProjectPage data={PROJECT_CONTENT[this.props.query.projectID]} />
-      </ThemeProvider>
+      <>
+        <Head>
+          <title>{`Mark Valentino - ${
+            PROJECT_CONTENT[this.props.query.projectID].title
+          }`}</title>
+          {getHeadBoilerPlate(
+            PROJECT_CONTENT[this.props.query.projectID].description
+          )}
+        </Head>
+        <ThemeProvider theme={MainTheme}>
+          <NavBar />
+          <ProjectPage data={PROJECT_CONTENT[this.props.query.projectID]} />
+        </ThemeProvider>
+      </>
     );
   }
 }
