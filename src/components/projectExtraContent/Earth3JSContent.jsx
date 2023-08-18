982bca6f52d43b79b5da41b4ad7ed242c6964214
Mark Valentino <mike9377mike9377@gmail.com>
2023-08-18 14:01:46 -0400

    finished typing extra content for Earth3JS

--- src/components/projectExtraContent/Earth3JSContent.jsx
+++ src/components/projectExtraContent/Earth3JSContent.jsx
@@ -5,16 +5,42 @@ import { Typography } from "@mui/material";
 export const Earth3JSContent = () => {
   return (
     <>
-      <Typography variant="h4" style={{ marginBottom: 10 }}>
+      <Typography variant="h4" gutterBottom>
         How it was coded
       </Typography>
-      <Typography variant="body1" color="text.secondary">
+      <Typography variant="body1" color="text.secondary" paragraph>
         I used{" "}
         <Link href="https://threejs.org/docs/#api/en/geometries/SphereGeometry">
           SphereGeometry
         </Link>{" "}
         components for coding the earth and moon. For the moon, it was quite
-        simple as I essentially needed a simple sphere. However, 
+        simple as I essentially needed a simple sphere. However, it wasn't so
+        easy with generating the earth as I wanted to have animated clouds that
+        would move across the earth's surface. I came up with the solution to
+        make a transparent sphere with the clouds texture that would encapsulate
+        the earth. This transparent sphere has a diameter of 2.005, and the
+        earth has a diameter of just 2.
+      </Typography>
+      <Typography paragraph>
+        Giving users the option to toggle between high and low settings was a
+        must have in my mind, so I created a clickable floating toggle button
+        that would turn off the advanced shading, lower polygon counts, and load
+        lower resolution textures.
+      </Typography>
+      <Typography paragraph>
+        I wanted to have all the objects in the scene be in motion. I was able
+        to have the the earth sphere, the clouds sphere, and the moons sphere
+        all rotate on their Y axises. Coding the moon to orbit to orbit the
+        earth was a challenge. I applied a trigonometric formula for orbital
+        motion to move the moon on its X and Z axises. The settings toggle had
+        oscillation applied to all of its axises in varying amounts to give the
+        impression it was floating in space.
+      </Typography>
+      <Typography paragraph>
+        I thought it would be neat to be able to change the simulation speed or
+        to just pause the simulation, so I implemented a speed control slider.
+        The value from the slider was placed as a coefficient in all animation
+        formulas.
       </Typography>
     </>
   );
--- src/utils/getProjectExtraContent.jsx
+++ src/utils/getProjectExtraContent.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import { PROJECT_TITLES } from "@/consts/projectContent";
 import { Earth3JSContent } from "@/components/projectExtraContent/Earth3JSContent";
 
+/**
+ * Function to get extra content associated with a project. This content is shown on
+ * the project's dedicated page. The extra content is not in the project's json object
+ * as it would keep load times on the projects page shorter.
+ * @param {*} title the title of the project to get content from.
+ * @returns extra project contenet in the form of a component.
+ */
 export const getExtraProjectContent = (title) => {
   switch (title) {
     case PROJECT_TITLES.Earth3Js:
-      return <Earth3JSContent/>;
+      return <Earth3JSContent />;
     default:
       return <></>;
   }
-
-}
+};
