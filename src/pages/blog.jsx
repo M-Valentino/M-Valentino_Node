3b2fddd46a6b1e37b8d396dc06ecbfbe5589465a
Mark Valentino <mike9377mike9377@gmail.com>
2023-10-29 12:28:00 -0400

    started making second blog post

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -35,6 +35,45 @@ export default function Home() {
         </MainHeading>
         <CustomPaper isDesktopView={isDesktopView}>
           <SubHeading shrinkFontOn={isLargeMobileView}>
+            I Don't Quite Like the Design of My Site
+          </SubHeading>
+          <Typography
+            variant="h6"
+            gutterBottom
+            color="secondary"
+            style={{ fontStyle: "italic" }}
+          >
+            Published October 15, 2023
+          </Typography>
+          <Typography paragraph>
+            I'm sure this sounds shocking to people reading this blog, but I'm
+            not the most fond of how my website looks. So, why did I make the
+            site look like this in the first place? The short answer is:
+            marketability.
+          </Typography>
+          <Typography paragraph>
+            My taste in design is kinda out there. I like maximalism instead of
+            minimalist. I like noncomfornity over conformity. There is nothing
+            wrong with my tastes, however, I can't just design a website
+            according to what I think looks good. Most people would either
+            dislike it or think of me as not having good web design skills.
+          </Typography>
+          <img src="/blogImages/neocities_professional_website.webp" style={{width: "100%"}}/>
+          <Typography paragraph>
+            People have told me my website looks great, and I know it looks
+            modern. The reason why is because I'm following web design trends,
+            and I'm using Material UI and custom components resembling what they
+            use. Part of me does like the design, but I feel a little empty
+            inside looking at it. To me my site looks boring, sterile, and not
+            unique enough. Part of it is because I haven't fully decorated it
+            all the way, but even when I achieve that, I still wouldn't fully be
+            proud of it.
+          </Typography>
+
+        </CustomPaper>
+
+        <CustomPaper isDesktopView={isDesktopView}>
+          <SubHeading shrinkFontOn={isLargeMobileView}>
             Could My Image Processing Library Replace MarvinJ?
           </SubHeading>
           <Typography
