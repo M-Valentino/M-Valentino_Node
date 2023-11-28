b41353a809bacf221c31f5b43a3876a1883b39d8
Mark Valentino <mike9377mike9377@gmail.com>
2023-11-27 22:44:17 -0500

    broke mobile menu and desktop menu into two components

---
+++ src/components/layout/navigation/DesktopMenu.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import UseAnimations from "react-useanimations";
+import github from "react-useanimations/lib/github";
+import linkedin from "react-useanimations/lib/linkedin";
+import { MUI_SECONDARY_COLOR_GRAY } from "@/consts/stylingValues";
+import { PAGE_TITLES } from "@/consts/pageTitles";
+import { NavButton } from "./NavButton";
+
+export const DesktopMenu = ({ activeLink, isDesktopView }) => {
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          width: 380,
+          margin: "auto",
+        }}
+      >
+        <NavButton
+          activeLink={activeLink}
+          buttonTextAndOrLink={PAGE_TITLES.home}
+        />
+        <NavButton
+          activeLink={activeLink}
+          buttonTextAndOrLink={PAGE_TITLES.projects}
+        />
+        <NavButton
+          activeLink={activeLink}
+          buttonTextAndOrLink={PAGE_TITLES.blog}
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          width: 240,
+          position: "absolute",
+          top: 0,
+          right: isDesktopView ? 50 : 5,
+        }}
+      >
+        {/* Wrapping NavButton inside UseAnimations ensures that
+          the animation plays when hovering on the button. */}
+        <UseAnimations
+          animation={linkedin}
+          size={30}
+          wrapperStyle={{ transform: "translate(-1px, -4.8px)" }}
+          strokeColor={MUI_SECONDARY_COLOR_GRAY}
+          render={(eventProps, animationProps) => (
+            <NavButton
+              externalLink
+              buttonTextAndOrLink={PAGE_TITLES.linkedIn}
+              events={eventProps}
+            >
+              <div {...animationProps} />
+            </NavButton>
+          )}
+        />
+        <UseAnimations
+          animation={github}
+          size={30}
+          wrapperStyle={{ transform: "translateY(-4px)" }}
+          strokeColor={MUI_SECONDARY_COLOR_GRAY}
+          render={(eventProps, animationProps) => (
+            <NavButton
+              externalLink
+              buttonTextAndOrLink={PAGE_TITLES.gitHub}
+              events={eventProps}
+            >
+              <div {...animationProps} />
+            </NavButton>
+          )}
+        />
+      </div>
+    </>
+  );
+};
--- src/components/layout/navigation/navbar.jsx
+++ src/components/layout/navigation/navbar.jsx
@@ -1,39 +1,20 @@
 import React from "react";
 import Link from "next/link";
-import {
-  Button,
-  Divider,
-  IconButton,
-  Menu,
-  useMediaQuery,
-} from "@mui/material/";
-import MenuIcon from "@mui/icons-material/Menu";
-import CloseIcon from "@mui/icons-material/Close";
-import UseAnimations from "react-useanimations";
-import github from "react-useanimations/lib/github";
-import linkedin from "react-useanimations/lib/linkedin";
-import {
-  MINUTE_SHADOW_SVG,
-  MUI_SECONDARY_COLOR_GRAY,
-} from "@/consts/stylingValues";
-import { PAGE_TITLES } from "@/consts/pageTitles";
-import { NavButton } from "./NavButton";
-import { MobileNavItem } from "./MobileNavItem";
+import { Button, useMediaQuery } from "@mui/material/";
+import { MINUTE_SHADOW_SVG } from "@/consts/stylingValues";
+import { MobileMenu } from "./MobileMenu";
+import { DesktopMenu } from "./DesktopMenu";
 
-// This component is the main navbar of the site.
-const NavBar = (props) => {
-  // The index of the button to change color to show it is the active link.
-  const { activeLink } = props;
+/**
+ * This component is the main navbar of the site and determines
+ * whether to show the mobile or desktop menu.
+ * @param {*} activeLink The index of the button (desktop) or
+ * menu item (mobile) to change color to show it is the active link.
+ * @returns component
+ */
+const NavBar = ({ activeLink }) => {
   const isDesktopView = useMediaQuery("(min-width:1200px)");
   const isMobileView = useMediaQuery("(max-width:876px)");
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
 
   return (
     <div style={{ marginBottom: 50 }}>
@@ -65,131 +46,9 @@ const NavBar = (props) => {
       </Button>
 
       {isMobileView ? (
-        <>
-          <IconButton
-            id="basic-button"
-            aria-controls={open ? "basic-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-            onClick={handleClick}
-            style={{ position: "absolute", top: 0, right: 0 }}
-          >
-            {open ? (
-              <CloseIcon
-                color="primary"
-                fontSize="large"
-                style={{ filter: MINUTE_SHADOW_SVG }}
-              />
-            ) : (
-              <MenuIcon
-                color="primary"
-                fontSize="large"
-                style={{ filter: MINUTE_SHADOW_SVG }}
-              />
-            )}
-          </IconButton>
-          <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              "aria-labelledby": "basic-button",
-            }}
-            style={{ textDecoration: "none" }}
-          >
-            <MobileNavItem
-              activeLink={activeLink}
-              itemTextAndOrLink={PAGE_TITLES.home}
-            />
-            <MobileNavItem
-              activeLink={activeLink}
-              itemTextAndOrLink={PAGE_TITLES.projects}
-            />
-            <MobileNavItem
-              activeLink={activeLink}
-              itemTextAndOrLink={PAGE_TITLES.blog}
-            />
-            <Divider />
-            <MobileNavItem
-              externalLink
-              itemTextAndOrLink={PAGE_TITLES.gitHub}
-            />
-            <MobileNavItem
-              externalLink
-              itemTextAndOrLink={PAGE_TITLES.linkedIn}
-            />
-          </Menu>
-          {/* For creating spacing below the navbar during mobile view. */}
-          <div style={{ width: "100%", height: 15 }}></div>
-        </>
+        <MobileMenu activeLink={activeLink} />
       ) : (
-        <>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-around",
-              width: 380,
-              margin: "auto",
-            }}
-          >
-            <NavButton
-              activeLink={activeLink}
-              buttonTextAndOrLink={PAGE_TITLES.home}
-            />
-            <NavButton
-              activeLink={activeLink}
-              buttonTextAndOrLink={PAGE_TITLES.projects}
-            />
-            <NavButton
-              activeLink={activeLink}
-              buttonTextAndOrLink={PAGE_TITLES.blog}
-            />
-          </div>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              width: 240,
-              position: "absolute",
-              top: 0,
-              right: isDesktopView ? 50 : 5,
-            }}
-          >
-            {/* Wrapping NavButton inside UseAnimations ensures that
-             the animation plays when hovering on the button. */}
-            <UseAnimations
-              animation={linkedin}
-              size={30}
-              wrapperStyle={{ transform: "translate(-1px, -4.8px)" }}
-              strokeColor={MUI_SECONDARY_COLOR_GRAY}
-              render={(eventProps, animationProps) => (
-                <NavButton
-                  externalLink
-                  buttonTextAndOrLink={PAGE_TITLES.linkedIn}
-                  events={eventProps}
-                >
-                  <div {...animationProps} />
-                </NavButton>
-              )}
-            />
-            <UseAnimations
-              animation={github}
-              size={30}
-              wrapperStyle={{ transform: "translateY(-4px)" }}
-              strokeColor={MUI_SECONDARY_COLOR_GRAY}
-              render={(eventProps, animationProps) => (
-                <NavButton
-                  externalLink
-                  buttonTextAndOrLink={PAGE_TITLES.gitHub}
-                  events={eventProps}
-                >
-                  <div {...animationProps} />
-                </NavButton>
-              )}
-            />
-          </div>
-        </>
+        <DesktopMenu activeLink={activeLink} isDesktopView={isDesktopView} />
       )}
     </div>
   );
