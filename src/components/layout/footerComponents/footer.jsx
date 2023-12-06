a32681942e51f90271c11af45de4eec200d6cf64
Mark Valentino <mike9377mike9377@gmail.com>
2023-12-06 15:52:47 -0500

    added website source code to footer

--- src/components/layout/footerComponents/footer.jsx
+++ src/components/layout/footerComponents/footer.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Link from "next/link";
-import { Button, Stack } from "@mui/material";
+import { Button, Grid } from "@mui/material";
 import { PrivacyPolicyDialog } from "./privacyPolicyDialog";
 
 /**
@@ -18,28 +18,46 @@ export const Footer = () => {
     setPrivacyPolicyOpen(false);
   };
   return (
-    <Stack direction="row" justifyContent="center">
-      <Button
-        LinkComponent={Link}
-        href="/humans.txt"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        humans.txt
-      </Button>
-      <Button onClick={() => setPrivacyPolicyOpen(true)}>Privacy Policy</Button>
-      <PrivacyPolicyDialog
-        handlePrivacyPolicyClose={handlePrivacyPolicyClose}
-        privacyPolicyOpen={privacyPolicyOpen}
-      />
-      <Button
-        LinkComponent={Link}
-        href="/sitemap.xml"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Site Map
-      </Button>
-    </Stack>
+    <Grid container direction="row" justifyContent="center" spacing={1}>
+      <Grid item>
+        <Button
+          LinkComponent={Link}
+          href="/humans.txt"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          humans.txt
+        </Button>
+      </Grid>
+      <Grid item>
+        <Button onClick={() => setPrivacyPolicyOpen(true)}>
+          Privacy Policy
+        </Button>
+        <PrivacyPolicyDialog
+          handlePrivacyPolicyClose={handlePrivacyPolicyClose}
+          privacyPolicyOpen={privacyPolicyOpen}
+        />
+      </Grid>
+      <Grid item>
+        <Button
+          LinkComponent={Link}
+          href="/sitemap.xml"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Site Map
+        </Button>
+      </Grid>
+      <Grid item>
+        <Button
+          LinkComponent={Link}
+          href="https://github.com/M-Valentino/M-Valentino_NextJS"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Website Source Code
+        </Button>
+      </Grid>
+    </Grid>
   );
 };
