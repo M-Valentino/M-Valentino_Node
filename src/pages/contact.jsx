08a0939f6654eb9d507a2c015fc91a9a0c6754c4
Mark Valentino <mike9377mike9377@gmail.com>
2023-12-08 03:24:56 -0500

    setup initial framework

---
+++ src/pages/contact.jsx
@@ -0,0 +1,55 @@
+import React, { useRef } from "react";
+import { MainWrapper } from "@/components/layout/MainWrapper";
+import { CustomPaper } from "@/components/layout/CustomPaper";
+import { MainHeading } from "@/components/layout/Headings";
+import { Button, TextField } from "@mui/material";
+export default function Contact() {
+  const messageRef = useRef(); 
+  const sendValue = () => {
+    if (messageRef.current.value.length > 1024 ) {
+      console.log("hjhjh");
+    } else {
+    console.log(messageRef.current.value);
+    }
+  };
+
+  return (
+    <MainWrapper>
+      <CustomPaper>
+        <MainHeading>Contact Me</MainHeading>
+        <form noValidate autoComplete="off">
+          <TextField
+            fullWidth
+            required
+            id="email"
+            label="Email address"
+            variant="outlined"
+            helperText=""
+            inputRef={messageRef}
+            style={{ marginTop: 20, maxWidth: 300 }}
+          />
+          <TextField
+            fullWidth
+            required
+            inputRef={messageRef}
+            id="standard-textarea"
+            label="Your message"
+            multiline
+            rows={12}
+            variant="outlined"
+            helperText="bjhb"
+            style={{ marginTop: 20 }}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            // endIcon={<SendIcon />}
+            onClick={sendValue}
+          >
+            Send
+          </Button>
+        </form>
+      </CustomPaper>
+    </MainWrapper>
+  );
+}
