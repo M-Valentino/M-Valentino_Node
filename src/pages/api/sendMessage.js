04a8074a132b9de0603182fe83b63e8a05da8800
Mark Valentino <mike9377mike9377@gmail.com>
2023-12-09 21:27:56 -0500

    added logic to check previous messages for duplicates

--- src/pages/api/sendMessage.js
+++ src/pages/api/sendMessage.js
@@ -9,6 +9,8 @@ import {
   checkMessageTooShort,
 } from "@/utils/validations";
 
+const previousMessage = await kv.lindex("emails", 0);
+const prevPrevMessage = await kv.lindex("emails", 1);
 
 export default function handler(request, response) {
   const email = nextBase64.decode(request.query.email);
@@ -21,8 +23,16 @@ export default function handler(request, response) {
     !checkMessageTooLong(message) &&
     !checkMessageTooShort(message)
   ) {
-    kv.lpush("emails", `${email}␟${message}␟${Date.now()}`)
-    return response.status(200).json({ message: "success" });
+    // Prevents the same message from being sent
+    if (
+      previousMessage.includes(`${email}␟${message}`) ||
+      prevPrevMessage.includes(`${email}␟${message}`)
+    ) {
+      return response.status(200).json({ message: "fail" });
+    } else {
+      kv.lpush("emails", `${email}␟${message}␟${Date.now()}`);
+      return response.status(200).json({ message: "success" });
+    }
   }
   return response.status(500);
 }
