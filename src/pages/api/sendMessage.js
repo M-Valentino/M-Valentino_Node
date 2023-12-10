4966e6af35c0ea482a955191ba072be6c3627111
Mark Valentino <mike9377mike9377@gmail.com>
2023-12-10 13:03:03 -0500

    refactored code and added commenting

--- src/pages/api/sendMessage.js
+++ src/pages/api/sendMessage.js
@@ -12,35 +12,39 @@ import {
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export default async function handler(request, response) {
-  // Prevents the same message from being sent
+  const { method } = request;
+  const email = nextBase64.decode(request.query.email);
+  const message = nextBase64.decode(request.query.message);
   const previousMail = await kv.lindex("emails", 0);
   const previousTimestamp = parseInt(
     previousMail.substring(previousMail.lastIndexOf("␟") + 1)
   );
   const now = Date.now();
   const timeDifference = now - previousTimestamp;
-  const { method } = request;
-  const email = nextBase64.decode(request.query.email);
-  const message = nextBase64.decode(request.query.message);
   const currentMailSubStr = `${email}␟${message}`;
 
   if (
+    /**
+     * I expect the contact form to be used maybe once every few days.
+     * Maybe ocaisionally people might forget to include
+     * something in their message and send something shortly after.
+     */
     timeDifference > 20000 &&
+    // Includes the same validations the frontent does.
     !checkEmailInvalid(email) &&
     !checkEmailTooLong(email) &&
     !checkHasGibberish(message) &&
     !checkMessageInvalid(message) &&
     !checkMessageTooLong(message) &&
     !checkMessageTooShort(message) &&
+    // For preventing the same message from being sent.
     !previousMail.includes(currentMailSubStr) &&
     method === "PUT"
   ) {
     await sleep(1000);
     kv.lpush("emails", `${currentMailSubStr}␟${Date.now()}`);
     return response.status(200).json({ message: "success" });
-  } else {
-    await sleep(10000);
   }
-
+  await sleep(10000);
   return response.status(200).json({ message: "fail" });
 }
