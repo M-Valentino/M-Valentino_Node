b14116751c72cfcd5933f7325ea778d1bbb4479c
Mark Valentino <mike9377mike9377@gmail.com>
2023-12-10 18:16:29 -0500

    resolved eslint issue

--- src/pages/api/getViews.js
+++ src/pages/api/getViews.js
@@ -6,13 +6,13 @@ import { formatForRedisKey } from "@/utils/linkFunctions";
  * Function to get number of views associated with a blog post and
  * increment them. The query validates if a title exists so that extra keys don't
  * get written to the redis db.
- * TODO resolve import/no-anonymous-default-export
  */
 export default async (request, response) => {
   const title = formatForRedisKey(request.query.title);
   const checkIfTitleExists = (obj) =>
     formatForRedisKey(obj.title) === title;
-  if (BLOG_POSTS_NO_COMPONENT.some(checkIfTitleExists)) {
+  const titleExists = BLOG_POSTS_NO_COMPONENT.some(checkIfTitleExists)
+  if (titleExists) {
     const views = await kv.incr(`${title}_views`);
     return response.status(200).json(Intl.NumberFormat("en-us").format(views));
   }
