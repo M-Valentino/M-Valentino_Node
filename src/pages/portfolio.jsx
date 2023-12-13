44017de717ada2d2aea4812c75adcb277a9991bf
Mark Valentino <mike9377mike9377@gmail.com>
2023-12-12 20:50:07 -0500

    got my portfolio up

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import ImageListItemBar from "@mui/material/ImageListItemBar";
-import ListSubheader from "@mui/material/ListSubheader";
 import IconButton from "@mui/material/IconButton";
 import InfoIcon from "@mui/icons-material/Info";
+import { Typography } from "@mui/material";
 import { CustomHead } from "@/components/layout/CustomHead";
 import { CustomPaper } from "@/components/layout/CustomPaper";
 import { MainWrapper } from "@/components/layout/MainWrapper";
+import { MainHeading } from "@/components/layout/Headings";
 
 export default function Portfolio() {
   return (
@@ -15,12 +16,15 @@ export default function Portfolio() {
       <CustomHead />
       <MainWrapper>
         <CustomPaper>
+          <MainHeading>Portfolio</MainHeading>
+          <Typography gutterBottom>
+            This is a curated showcase of my technical art in AutoCAD and Inventor.
+          </Typography>
           <ImageList sx={{ width: "100%" }}>
             {itemData.map((item) => (
-              <ImageListItem key={item.img}>
+              <ImageListItem key={item.name}>
                 <img
-                  srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                  src={`${item.img}?w=248&fit=crop&auto=format`}
+                  src={`/portfolioImages/${item.name}`}
                   alt={item.title}
                   loading="lazy"
                 />
@@ -47,74 +51,23 @@ export default function Portfolio() {
 
 const itemData = [
   {
-    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    name: "cad_car_isometric.png",
     title: "Breakfast",
     author: "@bkristastucchio",
-    rows: 2,
-    cols: 2,
-    featured: true,
   },
   {
-    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    name: "comming_soon_to_a_store_near_you.png",
     title: "Burger",
     author: "@rollelflex_graphy726",
   },
   {
-    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    name: "headphones_housing_inventor_assembly.png",
     title: "Camera",
     author: "@helloimnik",
   },
   {
-    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    name: "sturdy_computer_desk.png",
     title: "Coffee",
     author: "@nolanissac",
-    cols: 2,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
-    title: "Hats",
-    author: "@hjrc33",
-    cols: 2,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
-    title: "Honey",
-    author: "@arwinneil",
-    rows: 2,
-    cols: 2,
-    featured: true,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
-    title: "Basketball",
-    author: "@tjdragotta",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
-    title: "Fern",
-    author: "@katie_wasserman",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
-    title: "Mushrooms",
-    author: "@silverdalex",
-    rows: 2,
-    cols: 2,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
-    title: "Tomato basil",
-    author: "@shelleypauls",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
-    title: "Sea star",
-    author: "@peterlaster",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
-    title: "Bike",
-    author: "@southside_customs",
-    cols: 2,
   },
 ];
