19b3cb0e8e6ec1ee03464edad846cc009ebaa2a5
Mark Valentino <mike9377mike9377@gmail.com>
2024-03-06 19:51:16 -0500

    added grayscale filter

--- src/pages/sacredOSRedirect.tsx
+++ src/pages/sacredOSRedirect.tsx
@@ -12,31 +12,39 @@ export default function SacredOSRedirect() {
           height: "100vh",
           backgroundImage: "url(projectImages/sacredOS.webp)",
           backgroundSize: "cover",
+          imageRendering: "pixelated",
           display: "flex",
           flexDirection: "row",
           justifyContent: "center",
+          filter: "grayscale(70%)",
         }}
+      ></div>
+      <Button
+        href="https://sacred.neocities.org"
+        target="_blank"
+        rel="noopener"
+        variant="contained"
+        style={{
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          transform: "translate(-50%, -50%)",
+          fontSize: "6.5vw",
+          whiteSpace: "nowrap",
+        }}
+        sx={{ boxShadow: 20 }}
+        endIcon={
+          <OpenInNewIcon
+            style={{
+              fontSize: "5.5vw",
+              transform: "translateY(-1.3vw)",
+              filter: SHADOWS.minuteSVG,
+            }}
+          />
+        }
       >
-        <Button
-          href="https://sacred.neocities.org"
-          target="_blank"
-          rel="noopener"
-          variant="contained"
-          style={{ margin: "auto", fontSize: "6.5vw" }}
-          sx={{ boxShadow: 20 }}
-          endIcon={
-            <OpenInNewIcon
-              style={{
-                fontSize: "5.5vw",
-                transform: "translateY(-1.3vw)",
-                filter: SHADOWS.minuteSVG,
-              }}
-            />
-          }
-        >
-          Launch Sacred OS
-        </Button>
-      </div>
+        Launch Sacred OS
+      </Button>
     </ThemeProvider>
   );
 }
