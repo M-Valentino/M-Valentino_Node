d6d6a788dae1107cbe87556c76b77ae69129f75c
Mark Valentino <mike9377mike9377@gmail.com>
2024-04-02 19:48:47 -0400

    fixed spellings

--- src/components/blogComponents/blogPosts/Post4.tsx
+++ src/components/blogComponents/blogPosts/Post4.tsx
@@ -10,6 +10,13 @@ export const Post4 = () => {
     5,
     BASE_64_IMAGES.binary.length - 2
   );
+  const menuCode = `<nav>
+    <topnav>
+      <mi href="/">Home</mi>
+      <mi href="/blog">Blog</mi>
+      <mi href="/contact">Contact</mi>
+    </topnav>
+  </nav>`;
   return (
     <>
       <Typography paragraph>
@@ -19,7 +26,7 @@ export const Post4 = () => {
         have a few improvements to HTML5 I'm sharing.
       </Typography>
       <Typography variant="h5" style={{ fontWeight: "bold" }}>
-        Enhanced Base64 compression
+        Enhanced Base64 Compression
       </Typography>
       <Typography paragraph>
         On the index page of my webpage I have a few base64 images. Two of them
@@ -79,7 +86,7 @@ export const Post4 = () => {
         />
       </div>
       <Typography paragraph>
-        '¡' is an upsite down exclamation point and the first visible character
+        '¡' is an upside down exclamation point and the first visible character
         in the extended ASCII table. It is set to be equal to the substring
         repeated five times. Now, if we insert '¡' into the base64 data, the
         data becomes shorter:
@@ -105,8 +112,15 @@ export const Post4 = () => {
         just declare another variable like '¢'. We have 94 single letter
         variables in the extended ASCII table to choose from.
       </Typography>
+      <Typography paragraph>
+        Since webpages are already typically compressed when being sent over
+        HTTP, this might not improve compression all that much, BUT it will
+        definitely improve code in codebases where developers won't have to
+        scroll so far to the right just to had a semi colon to the end of a base
+        64 image string like I had to do when making this blog post.
+      </Typography>
       <Typography variant="h5" style={{ fontWeight: "bold" }}>
-        Incorporating the Hamburger Menu
+        Make the Hamburger Menu Easier to Code.
       </Typography>
       <Typography paragraph>
         The hamburger menu has become a defacto standard in designing mobile
@@ -128,9 +142,65 @@ export const Post4 = () => {
         a thing with JavaScript. It used to be relatively easy to make an
         acceptable simple website in the early days of the internet because
         people only had to worry about coding for desktop users. I believe HTML
-        has some catching up to do with ease of use. That is why I propse there
+        has some catching up to do with ease of use. That is why I propose there
         should a new HTML element that will easily create a responsive menu with
         JavaScript and CSS being optional.
+      </Typography>
+      <Typography paragraph>
+        Here is how my solution works. A new element called "topnav" will be
+        created. topnav elements contain "mi"s which is menu item for short.
+        They will simply be text with an optional href or JavaScript event.
+      </Typography>
+      <div
+        style={{
+          fontFamily: "monospace",
+          overflowY: "scroll",
+          marginBottom: 15,
+        }}
+      >
+        <CodeBlock
+          text={menuCode}
+          language={"javascript"}
+          theme={dracula}
+          showLineNumbers={true}
+          wrapLongLines
+        />
+      </div>
+      <Typography paragraph>
+        On desktop screens, it will appear simply as text with underlines
+        arranged in a row with some padding between the menu items. If the coder
+        wanted the menu to be vertical, a new CSS rule can be applied to change
+        the orientation.
+      </Typography>
+      <Typography paragraph>
+        On mobile screens, the menu will be aligned to the right of the screens
+        by default and would show a typical hamburger menu icon. Potentially it
+        could just be non-selectable text containing this character: "☰", the
+        "Trigram for Heaven". Having the icon be text by default would allow for
+        browser developers to easily code it into their rendering engine. since
+        the hamburger icon could be styled to have a different color, have a
+        shadow, etc. When the hamburger menu icon is clicked, simple white box
+        below containing the menu items is displayed with each menu item ordered
+        vertically.
+      </Typography>
+      <Typography paragraph>
+        Now here is the really interesting part. In order for the switch to
+        happen from desktop menus to mobile hamburger menus, the browser chooses
+        when this happens. On mobile Safari or on Chrome for Android, the
+        browser can just choose to show the hamburger menu. No @media rules are
+        necessary, although it would be a good idea to provide CSS options to
+        override the default browser behavior.
+      </Typography>
+      <Typography paragraph>
+        This is a real simple solution. Obviously there would need to be a lot
+        of additions to the CSS standard being made, but implementing these new
+        elements would make HTML5 easier for beginners. The hamburger menu is
+        more common on websites than tables or even{" "}
+        <SmartLink href="https://www.w3schools.com/tags/tag_meter.asp">
+          the obscure meter tag
+        </SmartLink>
+        , so I believe there is a strong case for it being added to the HTML
+        standard.
       </Typography>
     </>
   );
