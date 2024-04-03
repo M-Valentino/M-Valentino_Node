37dc56a1c5bec70d1d65090bfb1d56c90d8dd778
Mark Valentino <mike9377mike9377@gmail.com>
2024-04-03 15:34:48 -0400

    converted to typescript

--- src/pages/api/message-inbox.ts
+++ src/pages/api/message-inbox.ts
@@ -1,24 +1,33 @@
 import { kv } from "@vercel/kv";
 import nextBase64 from "next-base64";
+import type { NextApiRequest, NextApiResponse } from "next";
 
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const sleep = (ms: number): Promise<void> =>
+  new Promise((resolve) => setTimeout(resolve, ms));
 
 /**
  * API function to return messages users sent.
- * @param {*} request 
- * @param {*} response 
+ * @param {*} request
+ * @param {*} response
  * @returns saved messages in the kv database in the specified range.
  */
-export default async function handler(request, response) {
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+): Promise<void> {
   const { headers, method } = request;
-  const numberOfMessages = request.query.numberOfMessages;
+  const numberOfMessages = request.query.numberOfMessages as string;
   // For slowing down anyone trying to brute force hack.
   await sleep(1500);
   if (
     method === "GET" &&
     nextBase64.decode(headers.authorization) === process.env.ADMIN_PASSWORD
   ) {
-    const messages = await kv.lrange("emails", 0, numberOfMessages);
+    const messages = await kv.lrange(
+      "emails",
+      0,
+      parseInt(numberOfMessages, 10)
+    );
     return response.status(200).json({ message: messages });
   }
   // Rate limit
